ffd690cd61b04f06ef40ae49fd5ea9c9dedb150c
Nithin Prathapan <nithinprathapan369@gmail.com>
2022-08-06 22:20:50 +0530

    route to home modified

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import React from 'react'
-import Header from './Components/Header'
-import Home from './Components/Home'
+import React from "react";
+import Header from "./Components/Header";
+import Home from "./Components/Home";
+import { BrowserRouter, Routes, Route, Router } from "react-router-dom";
 
 function App() {
   return (
     <div>
-      <Header/>
-      <Home/>
+      <Header />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/home" element={<Home />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
