6d2073afefc4db32b6fce103b7dd5886ba72b9e7
Nithin Prathapan <nithinprathapan369@gmail.com>
2022-08-06 18:30:33 +0530

    created products.js

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import styled from 'styled-components'
+import Banner from './Banner'
+
 function Home() {
   return (
-    <Container>
+    <Banner/>
     
-    </Container>
+   
   )
 }
 
