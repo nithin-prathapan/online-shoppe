b8638b485dde10576dd767c97ce2ca804543e5f3
Nithin Prathapan <nithinprathapan369@gmail.com>
2022-08-07 20:32:24 +0530

    login.js design updated

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -4,8 +4,19 @@ import React from "react";
 function Login() {
   return (
     <Container>
-      <h2>User ddLogin</h2>
-      <LoginContainer></LoginContainer>
+      <h2>User Login</h2>
+      <LoginContainer>
+        <h5>Enter your Email</h5>
+        <input type="email" name="" id="" placeholder="Email..." />
+        <h5>Enter your Password</h5>
+        <input type="password" name="" id="" placeholder="Password..." />
+        <Button>Login</Button>
+        <SignUpDiv>
+          <p>
+            Don't have an accoount? <a href="/signup">click here to create a new acount</a>
+          </p>
+        </SignUpDiv>
+      </LoginContainer>
     </Container>
   );
 }
@@ -13,16 +24,50 @@ function Login() {
 export default Login;
 
 const Container = styled.div`
-  margin-top: 70px;
-  background: blue;
-  width: 100%;
-  height: 100%;
   display: flex;
+  margin-top: 70px;
   justify-content: center;
+  flex-direction: column;
   h2 {
     text-align: center;
   }
 `;
 const LoginContainer = styled.div`
-  
+  width: 200px;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  border: 1px solid #111;
+  padding: 30px;
+  justify-content: center;
+  margin: auto;
+  background: #111;
+  color: white;
+  border-radius: 5px;
+  border: 1px solid silver;
+
+  h5 {
+    text-align: left;
+  }
+  input {
+    margin-top: -18px;
+    margin-bottom: 10px;
+  }
+`;
+const Button = styled.button`
+  margin-top: 5px;
+  width: 100%;
+  margin: auto;
+  background: yellow;
+  border-radius: 5px;
+`;
+const SignUpDiv = styled.div`
+text-align:center;
+margin-top:0;
+font-size:10px;
+a{
+  color: rgb(10, 150, 255);;
+
+}
+
 `;
